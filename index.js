1026da0fb7310bb74f7acd88148321297eeb5a9a
jerin-arpa <ismatjerrin3@gmail.com>
2023-10-30 02:52:58 +0600

    Set token to http only cookie

--- index.js
+++ index.js
@@ -43,7 +43,13 @@ async function run() {
             const user = req.body;
             console.log(user);
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
-            res.send(token);
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: false,
+                    sameSite: 'none',
+                })
+                .send({ success: true });
         });
 
 
